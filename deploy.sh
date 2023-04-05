26a58b3c8c66204cf1c9c5068f342f1fe29e94e4
CreeGunning <creenike@gmail.com>
2023-04-05 14:29:41 +0100

    added continuous delivery

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+sudo apt update && sudo apt install nodejs npm
+
+sudo npm install -g pm2
+
+pm2 stop proj_app
+
+cd CreeDevOpsSecProject/
+
+npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
+
+pm2 start ./bin/www --name proj_app
