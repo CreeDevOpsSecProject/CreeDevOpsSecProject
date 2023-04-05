95bba2327cfd43b5e247c55f219a3af325399e94
CreeGunning <creenike@gmail.com>
2023-04-05 19:15:14 +0100

    ls

--- deploy.sh
+++ deploy.sh
@@ -4,17 +4,19 @@ sudo apt update && sudo apt install nodejs npm
 sudo npm install -g pm2
 
 pm2 stop proj_app
-
+echo "first ls"
 ls
 
 cd CreeDevOpsSecProject/
 
+echo "second ls"
 ls
 
 npm install -f
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
 
+echo "third ls"
 ls
 
 pm2 start ./bin/www --name proj_app
